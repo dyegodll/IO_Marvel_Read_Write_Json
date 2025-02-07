4e9aebb916ddeddaf5c87e2b37088277e984ba8a
Dyego Lima <dyegodll18@gmail.com>
2025-02-07 12:31:06 -0300

    usando DLL ExportarDados para transformar a lista em arquivo XML

--- IO_Read_Write_Json/Program.cs
+++ IO_Read_Write_Json/Program.cs
@@ -1,16 +1,27 @@
-﻿using PersonagemServices;
+﻿using ExportarDados;
+using PersonagemServices;
 using PersonagensMarvel;
 
-string path = "C:/Users/dyego.lima/source/repos/IO_Read_Write_Json/IO_Read_Write_Json/bin/Debug/net8.0";
 
-string doc1 = "/personagens_marvel.txt";
-string pathDoc1 = $"{path}{doc1}";
-List<PersonagemMarvel> lista = PersonagemMarvelServices.LerArquivoPersonagem(pathDoc1);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = "C:/Users/dyego.lima/source/repos/IO_Read_Write_Json/IO_Read_Write_Json/bin/Debug/net8.0";
 
-string doc2 = "/personagens_marvel.csv";
-string pathDoc2 = $"{path}{doc2}";
-PersonagemMarvelServices.EscreverArquivoPersonagem(pathDoc2, lista);
+        string doc1 = "/personagens_marvel.txt";
+        string pathDoc1 = $"{path}{doc1}";
+        List<PersonagemMarvel> lista = PersonagemMarvelServices.LerArquivoPersonagem(pathDoc1);
 
-string doc3 = "/personagens_marvel.json";
-string pathDoc3 = $"{path}{doc3}";
-PersonagemMarvelServices.EscreverArquivoJsonPersonagem(pathDoc3,lista);
+        string doc2 = "/personagens_marvel.csv";
+        string pathDoc2 = $"{path}{doc2}";
+        PersonagemMarvelServices.EscreverArquivoPersonagem(pathDoc2, lista);
+
+        string doc3 = "/personagens_marvel.json";
+        string pathDoc3 = $"{path}{doc3}";
+        PersonagemMarvelServices.EscreverArquivoJsonPersonagem(pathDoc3, lista);
+
+        var formatoArquivo = FormatoArquivos.Xml;
+        ExportarDados<PersonagemMarvel>.SalvarDados(path, formatoArquivo, lista);
+    }
+}
--- PersonagemServices/obj/Debug/net8.0/IO_Read_Write_Json.Services.AssemblyInfo.cs
+++ PersonagemServices/obj/Debug/net8.0/IO_Read_Write_Json.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IO_Read_Write_Json.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feb94377e9d60109457ebd55baea42c43a8c0e39")]
 [assembly: System.Reflection.AssemblyProductAttribute("IO_Read_Write_Json.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IO_Read_Write_Json.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
